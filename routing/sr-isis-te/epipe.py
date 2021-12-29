eba3e644db8b3f2a6c1e8cad18d0e6b5dbbe6137
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-12-28 22:10:04 -0600

    Updated

--- routing/sr-isis-te/epipe.py
+++ routing/sr-isis-te/epipe.py
@@ -6,12 +6,16 @@ def post_transform(topo: Box) -> None:
   """
   Processes links with 'epipe' attribute, and resolves to the loopback IP of the peer
   """
-  print( "JvB epipe post_transform:" )
+  print( f"JvB epipe post_transform" )
 
   # Need to modify node.links, not global topo.links
-  for node in topo.nodes:
+  for node in topo.nodes.values():
    for link in node.links: # topo.links:
     if 'epipe' in link:
-       peer = topo.nodes_map[ link.epipe ].loopback.ipv4
+       peer = topo.nodes[ link.epipe ].loopback.ipv4
        print( f"JvB: Resolved {link.epipe} to {peer}" )
        link.epipe_peer = peer
+
+  # Check consistency of models
+  # print( f"POST: nodes={topo.nodes}" )
+  # print( f"POST: links={topo.links}" )
