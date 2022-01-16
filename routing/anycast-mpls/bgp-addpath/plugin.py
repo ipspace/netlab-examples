0acd15aa0409651f7029870494480561cb85960e
Ivan Pepelnjak <ip@ipspace.net>
2022-01-16 10:07:01 +0100

    BGP anycast plugins adjusted to the new data structrures

--- routing/anycast-mpls/bgp-addpath/plugin.py
+++ routing/anycast-mpls/bgp-addpath/plugin.py
@@ -15,7 +15,7 @@ def init(topo: Box) -> None:
 
 def post_transform(topo: Box) -> None:
   config_name = api.get_config_name(globals())
-  for node in topo.nodes:
+  for name,node in topo.nodes.items():
     if 'add_path' in node.get('bgp',{}):
       api.list_attribute(node.bgp,'add_path',f'nodes.{node.name}.bgp')
       api.node_config(node,config_name)
--- routing/anycast-mpls/bgp-anycast/plugin.py
+++ routing/anycast-mpls/bgp-anycast/plugin.py
@@ -4,10 +4,7 @@ Custom plugin for BGP anycast scenario
 Removes IBGP sessions from the members of the 'anycast' group
 '''
 
-import sys
 from box import Box
-from netsim.augment import topology
-from netsim import common
 from netsim import api
 
 def init(topo: Box) -> None:
@@ -15,7 +12,7 @@ def init(topo: Box) -> None:
 
 def post_transform(topo: Box) -> None:
   config_name = api.get_config_name(globals())
-  for node in topo.nodes:
+  for name,node in topo.nodes.items():
     if 'anycast' in node.get('bgp',{}):
       node.bgp.advertise_loopback = False
       node.bgp.neighbors = [ n for n in node.bgp.neighbors if n.type != 'ibgp' ]
