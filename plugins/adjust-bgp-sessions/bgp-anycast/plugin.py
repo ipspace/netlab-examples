db054de79f0814ce59b47cfe1f5f6f7281894ebe
Ivan Pepelnjak <ip@ipspace.net>
2022-04-29 18:20:45 +0200

    netsim-tools plugin examples

---
+++ plugins/adjust-bgp-sessions/bgp-anycast/plugin.py
@@ -0,0 +1,17 @@
+'''
+Custom plugin for BGP anycast scenario
+
+Removes IBGP sessions from the members of the 'anycast' group
+'''
+
+from box import Box
+from netsim import api
+
+def init(topo: Box) -> None:
+  topo.defaults.bgp.attributes.node.append('anycast')
+
+def post_transform(topo: Box) -> None:
+  for name,node in topo.nodes.items():
+    if 'anycast' in node.get('bgp',{}):
+      node.bgp.advertise_loopback = False
+      node.bgp.neighbors = [ n for n in node.bgp.neighbors if n.type != 'ibgp' ]
---
+++ plugins/adjust-config-template/bgp-addpath/plugin.py
@@ -0,0 +1,20 @@
+'''
+Custom plugin for BGP anycast scenario
+
+Removes IBGP sessions from the members of the 'anycast' group
+'''
+
+import sys
+from box import Box
+from netsim.augment import topology
+from netsim import common
+from netsim import api
+
+def init(topo: Box) -> None:
+  topo.defaults.bgp.attributes.node.append('add_path')
+
+def post_transform(topo: Box) -> None:
+  config_name = api.get_config_name(globals())
+  for name,node in topo.nodes.items():
+    if 'add_path' in node.get('bgp',{}):
+      api.node_config(node,config_name)
