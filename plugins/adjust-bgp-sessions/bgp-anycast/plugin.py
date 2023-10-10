4a5708ac3392ee8fe6f399435bf29a469b06f3e2
Ivan Pepelnjak <ip@ipspace.net>
2023-10-10 17:56:16 +0200

    Migrate plugin to netlab release 1.6.3

--- plugins/adjust-bgp-sessions/bgp-anycast/plugin.py
+++ plugins/adjust-bgp-sessions/bgp-anycast/plugin.py
@@ -7,11 +7,11 @@ Removes IBGP sessions from the members of the 'anycast' group
 from box import Box
 from netsim import api
 
-def init(topo: Box) -> None:
-  topo.defaults.bgp.attributes.node.anycast = { 'type': 'ipv4', 'use': 'prefix' }
+_config_name: str = ''
 
 def post_transform(topo: Box) -> None:
   for name,node in topo.nodes.items():
     if 'anycast' in node.get('bgp',{}):
       node.bgp.advertise_loopback = False
       node.bgp.neighbors = [ n for n in node.bgp.neighbors if n.type != 'ibgp' ]
+      api.node_config(node,_config_name)
