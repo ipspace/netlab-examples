c8d06157dbeaf914824773ae59fcdac4836ec690
Ivan Pepelnjak <ip@ipspace.net>
2024-10-24 14:04:50 +0200

    EVPN designs: EBGP over EBGP

---
+++ EVPN/ebgp-ebgp/README.md
@@ -0,0 +1,15 @@
+# EVPN Designs: EBGP Everywhere
+
+This directory contains the lab topology described in the [EVPN Designs:
+EBGP-over-EBGP ](https://blog.ipspace.net/2024/10/evpn-designs-ebgp-ebgp/) blog
+post. The switches run EBGP (IPv4 AF) on physical interfaces and EBGP (EVPN AF)
+between lopback interfaces advertised via EBGP IIPv4 AF.
+
+![](http://blog.ipspace.net/2024/04/evpn-design-fabric.png)
+
+The lab topology uses a custom EBGP ECMP configuration template that is provided
+for Arista EOS and FRRouting and reports a warning if you're using another
+device for the leaf switches.
+
+If you need a similar template for another platform, please add the
+configuration template to the `ebgp.ecmp` directory and submit a Pull Request.
---
+++ EVPN/ebgp-ebgp/check.config/plugin.py
@@ -0,0 +1,54 @@
+import os
+
+from netsim import __version__
+from netsim.utils import log, files as _files,strings
+from netsim import data
+
+from box import Box
+
+def init(topology: Box) -> None:
+  if __version__ >= '1.9.2':
+    return
+
+  if topology.defaults.device not in ['eos','frr']:
+    log.error(
+      f'EBGP ECMP configuration is not available for device {topology.defaults.device}',
+      category=Warning,
+      module='custom')
+    topology.groups.get('leafs',{}).pop('config',None)
+
+def cleanup(topology: Box) -> None:
+  missing = data.get_empty_box()
+  for ndata in topology.nodes.values():
+    if not 'config' in ndata:
+      continue
+
+    candidate_files = [ 
+      f'{ndata.name}.{ndata.device}.j2',
+      f'{ndata.name}.j2',
+      f'{ndata.device}.j2' ]
+
+    for cfg in list(ndata.config):
+      for cfg_candidate in candidate_files:
+        cfg_file = _files.find_file(f'{cfg}/{cfg_candidate}',topology.defaults.paths.custom.dirs)
+      
+        if cfg_file:
+          break
+
+      if cfg_file:
+        break
+      
+      cfg_id = strings.make_id(cfg)
+      missing[cfg_id].item = cfg
+      data.append_to_list(missing[cfg_id].devices,ndata.device,ndata.name)
+      ndata.config = [ f for f in ndata.config if f != cfg ]
+
+  hint = "You will have to configure the missing functionality yourself"
+  for m_config in missing.values():
+    for m_device,m_nodes in m_config.devices.items():
+      log.error(
+        f'Custom configuration {m_config.item} is missing for device {m_device} (nodes {",".join(m_nodes)})',
+        category=Warning,
+        more_hints=hint,
+        module='check.config')
+      hint = None
